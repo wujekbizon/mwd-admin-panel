1ccfd3dac180a5bdb7e84d9b57974e3135277732
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-06-24 15:21:58 +0200

    heroku stack change from 20 to 22

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const herokuApi = 'https://adminpanelmwd.herokuapp.com/api/';
-const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = 'https://adminpanelmwd.herokuapp.com/api/';
+// const BASE_URL = 'http://localhost:5000/api/';
 
 const TOKEN = JSON.parse(
   JSON.parse(localStorage.getItem('persist:root'))?.user || '{}'
